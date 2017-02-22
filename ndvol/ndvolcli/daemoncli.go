c1e3d3d4e58e7dbacede7144582ff4420522f5da
Qeas <alexey.khodos@nexenta.com>
2017-02-22 20:54:00 +0300

    revert to non-background mode

--- ndvol/ndvolcli/daemoncli.go
+++ ndvol/ndvolcli/daemoncli.go
@@ -2,10 +2,7 @@ package ndvolcli
 
 import (
 	"github.com/urfave/cli"
-	ndvolDaemon "github.com/qeas/nedge-docker-volume/ndvol/daemon"
 	"github.com/sevlyar/go-daemon"
-	log "github.com/Sirupsen/logrus"
-	"syscall"
 )
 
 var (
@@ -33,62 +30,14 @@ var (
 		},
 		Action: cmdDaemonStart,
 	}
-
-	DaemonStopCmd = cli.Command{
-		Name: "stop",
-		Usage: "Stop the Nedge Docker Daemon: `stop",
-		Action: cmdDaemonStop,
-	}
 )
 
-func cmdDaemonStop(c *cli.Context) {
-	cntxt := &daemon.Context{
-		PidFileName: "/opt/nedge/var/run/ndvol.pid",
-		PidFilePerm: 0644,
-		LogFileName: "/opt/nedge/var/log/ndvol.log",
-		LogFilePerm: 0640,
-		Umask:       027,
-	}
-	d, err := cntxt.Search()
-	if err != nil {
-		log.Fatalln("Unable to send signal to the daemon:", err)
-	}
-	d.Signal(syscall.SIGTERM)
-}
 
 func cmdDaemonStart(c *cli.Context) {
-	cntxt := &daemon.Context{
-		PidFileName: "/opt/nedge/var/run/ndvol.pid",
-		PidFilePerm: 0644,
-		LogFileName: "/opt/nedge/var/log/ndvol.log",
-		LogFilePerm: 0640,
-		Umask:       027,
-	}
-	d, err := cntxt.Reborn()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer cntxt.Release()
-	if d != nil {
-		return
-	}
-
-	log.Info("- - - - - - - - - - - - - - -")
-	log.Info("Daemon started")
-	go DaemonStart(c)
-
-	err = daemon.ServeSignals()
-	if err != nil {
-		log.Info("Error:", err)
-	}
-	log.Info("Daemon terminated")
-}
-
-func DaemonStart(c *cli.Context) {
 	verbose := c.Bool("verbose")
 	cfg := c.String("config")
 	if cfg == "" {
 		cfg = "/opt/nedge/etc/ccow/ndvol.json"
 	}
-	ndvolDaemon.Start(cfg, verbose)
+	daemon.Start(cfg, verbose)
 }
