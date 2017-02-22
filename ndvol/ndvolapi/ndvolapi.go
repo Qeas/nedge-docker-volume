0a9444267b497029308698edab36d56e8b7ff43f
Qeas <alexey.khodos@nexenta.com>
2017-02-22 22:41:55 +0300

    typo

--- ndvol/ndvolapi/ndvolapi.go
+++ ndvol/ndvolapi/ndvolapi.go
@@ -175,7 +175,7 @@ func (c *Client) CreateVolume(name string, options map[string]string) (err error
 
 	mkfscmd := fmt.Sprintf("mkfs.%s", fstype)
 	if out, err := exec.Command(mkfscmd, nbd).CombinedOutput(); err != nil {
-		log.Infp("Error running mkfs command: ", err, "{", string(out), "}")
+		log.Info("Error running mkfs command: ", err, "{", string(out), "}")
 		msg := fmt.Sprintf("%s: %s", err, out)
 		if strings.Contains(msg, "executable file not found") {
 			if out, err := exec.Command(mkfscmd, nbd).CombinedOutput(); err != nil {
